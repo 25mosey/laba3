4e0cc7ae02104a7e976d6bcbc2679e3e2982d9ec
Juria1005 <shami1987@yandex.ru>
2022-06-11 11:19:36 +0200

    Добавьте файлы проекта.

---
+++ laba3/Drob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    internal class Drob
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Drob (double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double Chislo()
+        {
+            return A*(B/C);
+        }
+    }
+}
